7bd06a1cb36b73290672fab2f37924c4674f1ec5
Cesar Pisa <126973782+devPisa@users.noreply.github.com>
2024-11-06 19:57:01 -0300

    Feature/agente (#17)
    * Feat: Add agent model class
    
    * Feat: Logic correction agent model class
    
    * Feat: Logic correction agent model class
    
    ---------
    
    Co-authored-by: devPisa <cesar.pisa@alunos.bpkedu.com.br>

---
+++ app/models/agent.py
@@ -0,0 +1,17 @@
+from typing import List, Dict
+
+class Agent:
+    def __init__(self, identifier: str, public_key: str,
+                 agent_type: str = None, issued_hashes: List[Dict] = None):
+        self.identifier = identifier
+        self.public_key = public_key
+        self.agent_type = agent_type if agent_type is not None else {} 
+        self.issued_hashes = issued_hashes if issued_hashes is not None else []
+
+    def to_dict(self):
+        return {
+            "identifier": self.identifier,
+            "public_key": self.public_key,
+            "agent_type": self.agent_type,
+            "issued_hashes": self.issued_hashes      
+        }
