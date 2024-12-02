e3fe99d6f8587b43377b7cf36d868d3fc00b967c
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2024-12-02 00:01:54 -0300

    Feat: Update readme

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from routes import register_routes
-
-app = Flask(_name_)
-
-register_routes(app)
-
-if _name_ == "_main_":
-  app.run(debug=True, host="0.0.0.0", port=5000)
